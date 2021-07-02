12bf58872c18647e52fd72b5f3bc8c2d6582c1d3
felix <felix.calderon89@hotmail.com>
2021-07-01 22:46:16 -0600

    Remove ignored files

--- SocialMedia.InfraStructure/Repositories/PostRepository.cs
+++ SocialMedia.InfraStructure/Repositories/PostRepository.cs
@@ -2,25 +2,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using SocialMedia.Core.Interfaces;
+using System.Threading.Tasks;
 
 namespace SocialMedia.InfraStructure.Repositories
 {
-    public class PostRepository
+    public class PostRepository : IPostRepository
     {
-        public IEnumerable<Post> GetPosts()
+        Task<IEnumerable<Post>> IPostRepository.GetPosts()
+        {
+            var Post = (Task<IEnumerable<Post>>)Enumerable.Range(0, 10).Select(x => new Post
             {
+                PostId = x,
+                Description = $"Descripcion{x}",
+                Date = DateTime.Now,
+                Image = $"https://misapis.com/{x}",
+                UserId = x * 2
 
-                var Post = Enumerable.Range(0, 10).Select(x=> new Post {
-                    PostId = x,
-                    Description = $"Descripcion{x}",
-                    Date = DateTime.Now,
-                    Image = $"https://misapis.com/{x}",
-                    UserId = x * 2
-            
-                });
+            });
 
-                return Post;
-            }
+            return Post;
+        }
     }
 }
