ef2e38b1c7cd33c391c152de7fbb157935bfc445
felix <felix.calderon89@hotmail.com>
2021-07-02 23:35:04 -0600

    Se realiza scaffolding de la BD sql server sobre la carpeta Data de InfaStructure
    Comando en la consola de Nuget para scaffold:
    Scaffold-DbContext "Server=LAPTOP-475L2Q22\SQLSERVER;Database=SocialMediadb;Integrated Security = true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Data

---
+++ SocialMedia.InfraStructure/Data/Comentario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMedia.InfraStructure.Data
+{
+    public partial class Comentario
+    {
+        public int IdComentario { get; set; }
+        public int IdPublicacion { get; set; }
+        public int IdUsuario { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Activo { get; set; }
+
+        public Publicacion IdPublicacionNavigation { get; set; }
+        public Usuario IdUsuarioNavigation { get; set; }
+    }
+}
---
+++ SocialMedia.InfraStructure/Data/Publicacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SocialMedia.InfraStructure.Data
+{
+    public partial class Publicacion
+    {
+        public Publicacion()
+        {
+            Comentario = new HashSet<Comentario>();
+        }
+
+        public int IdPublicacion { get; set; }
+        public int IdUsuario { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+
+        public Usuario IdUsuarioNavigation { get; set; }
+        public ICollection<Comentario> Comentario { get; set; }
+    }
+}
